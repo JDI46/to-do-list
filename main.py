754a440873c5d30ef5b147e01f29e22bf2a9c9f3
Xander Whitby <xwhitby20@gmail.com>
2024-07-11 21:20:37 -0600

    made class method to view tasks

--- main.py
+++ main.py
@@ -1,19 +1,38 @@
+
+
+#class takes new task and adds to list
+
+
 class ToDoList:
-    list = []
+  
+  list = []
+
+  @classmethod
+  def add_task(cls):
+    while True:
+      add = input("New Task: ")
+      if add.lower() == 'stop':
+          break
+      cls.list.append(add)
+      
+
+  @classmethod
+  def remove_task(cls):
+    remove_prompt = input("Delete: ")
+    if remove_prompt.isdigit():
+      cls.list.remove(remove_prompt)
+
+
+  @classmethod
+  def view_tasks(cls):
+    for i in cls.list:
+      print(i)
+
+
+ToDoList.add_task()
+# ToDoList.remove_task()
+ToDoList.view_tasks()
+
 
     
-    def __init__(self, new_task):
-        self.new_task = new_task
-
-    def remove_task(self):
-        list.remove(self.new_task)
-
-    def view_task(self):
-        print(list)
-
-
-def create_task(new_task):
-    print(new_task)
-
-
-create_task(str(input(" ")))
+    
